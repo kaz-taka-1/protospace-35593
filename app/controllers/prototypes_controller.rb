656fa0556607a3cdc90f6832f27aadb0d8283ff1
kaz-taka-1 <k.t.niconico.physics@gmail.com>
2021-05-07 14:21:38 +0900

    first commit

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,57 @@
+class PrototypesController < ApplicationController
+  before_action :authenticate_user!, only:[ :new, :create, :edit, :update,:destroy] 
+  def index
+    @prototypes = Prototype.all
+  end
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
+      redirect_to prototypes_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @comment = Comment.new
+    @prototype = Prototype.find(params[:id])
+    @comments = @prototype.comments.includes(:user)
+  end
+
+  def edit
+    @prototype = Prototype.find(params[:id])
+
+    if current_user.id != @prototype.user_id
+      @prototypes = Prototype.all
+      render :index
+    end
+    
+  end
+  def update
+    @prototype = Prototype.find(params[:id])
+    if @prototype.update(prototype_params)
+      redirect_to prototype_path(@prototype.id)
+    else
+      render :edit
+    end
+  end
+  def destroy
+    @prototype = Prototype.find(params[:id])
+    @prototype.destroy
+    redirect_to prototypes_path
+  end
+
+  private
+
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image,).merge(user_id: current_user.id)
+  end
+  
+  
+
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,10 @@
+class UsersController < ApplicationController
+  def show
+    prototype = Prototype.find(params[:id])
+    @user = User.find(prototype.user_id)
+    @prototypes = @user.prototypes
+
+  end
+
+  
+end
